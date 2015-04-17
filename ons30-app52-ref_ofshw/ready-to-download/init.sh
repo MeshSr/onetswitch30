8be208ee588e0aa94aa6e85cc4f2b56b75f0ff99
Phil Zhao <if_blue@sina.com>
2015-04-17 11:01:21 +0800

    update init.sh

--- ons30-app52-ref_ofshw/ready-to-download/init.sh
+++ ons30-app52-ref_ofshw/ready-to-download/init.sh
@@ -1,12 +1,33 @@
-#!/bin/ash
+#!/bin/sh
 
-echo "Networking Initial, Please wait ..."
+switch_ip=10.0.0.10
+gateway_ip=10.0.0.1
+controller_ip=10.0.0.1
+controller_port=6633
+local_ip=127.0.0.1
+local_port=6632
+
+ofs_dir="/root/ofs-hw"
+interfaces="eth1,eth2,eth3,eth4"
+ofdatapath_options="--no-slicing"
+ofprotocol_options="--inactivity-probe=90"
+
+#--datapath-id must be exactly 12 hex digits
+id_num=`echo $switch_ip | awk -F '.' '{print $4}'`
+if [ $id_num -gt 99 ]
+then
+    datapath_id=000000000$id_num
+else
+    datapath_id=0000000000$id_num
+fi
+    
+echo "Networking Initial, Please wait..."
 # Due to driver issue, networking interfaces should be set up, then down at first.
 ifconfig eth1 up
 ifconfig eth2 up
 ifconfig eth3 up
 ifconfig eth4 up
-sleep 3
+sleep 1
 
 ifconfig eth0 down
 ifconfig eth1 down
@@ -19,30 +40,54 @@ ifconfig eth1 hw ether 00:0a:35:01:15:01
 ifconfig eth2 hw ether 00:0a:35:01:15:02
 ifconfig eth3 hw ether 00:0a:35:01:15:03
 ifconfig eth4 hw ether 00:0a:35:01:15:04
+sleep 1
 
-sleep 3
-
-ifconfig eth0 10.0.0.15/24
-
+ifconfig eth0 $switch_ip up
 ifconfig eth1 up
 ifconfig eth2 up
 ifconfig eth3 up
 ifconfig eth4 up
 ifconfig lo up
+sleep 1
+
+ret=`route | grep "default" | awk '{print $1}'`
+if [ ! $ret ]
+then
+    route add default gw $gateway_ip
+fi
+
+echo "Network Interfaces Initial Done"
+
+if [ ! -d $ofs_dir ]
+then
+    echo "ERROR: Please copy ofs binaries to the directory \"$ofs_dir\""
+    exit
+fi
+
+echo "Entering ofs directory \"$ofs_dir\"..."
+cd $ofs_dir
+
+echo "Starting configuring udatapath..."
+./udatapath/ofdatapath --datapath-id=$datapath_id --interfaces=$interfaces ptcp:$local_port $ofdatapath_options &
+sleep 3
+ret=`ps | grep "ofdatapath" | awk '{print $6}'`
+if [ ! $ret ]
+then
+    echo "ERROR: Excuting ofdatapath failed, please check..."
+    exit
+fi
 
 sleep 3
-echo "@++< Initial Done"
 
-route add default gw 10.0.0.1
-#cd /root
-#./ofup
+echo "Starting configuring secure channel..."
+./secchan/ofprotocol tcp:$local_ip:$local_port tcp:$controller_ip:$controller_port $ofprotocol_options &
+sleep 3
+ret=`ps | grep "ofprotocol" | awk '{print $6}'`
+if [ ! $ret ]
+then
+    echo "ERROR: Excuting ofprotocol failed, please check..."
+    exit
+fi
 
-cd /root/ofsoftswitch0929
-./udatapath/ofdatapath --datapath-id=000000000015 --interfaces=eth1,eth2,eth3,eth4 ptcp:6632 --no-slicing &
-sleep 5
-./secchan/ofprotocol  tcp:127.0.0.1:6632 tcp:10.0.0.1:6633 --inactivity-probe=90 &
-sleep 5
-ifconfig eth0 up
-
-
+echo "Openflow Switch Configured Succeessfully"
 
