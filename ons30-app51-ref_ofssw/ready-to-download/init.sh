c5708a974773d216482a8745c435a9f07a073498
Phil Zhao <if_blue@sina.com>
2015-06-24 10:40:31 +0800

    Add gateway config

--- ons30-app51-ref_ofssw/ready-to-download/init.sh
+++ ons30-app51-ref_ofssw/ready-to-download/init.sh
@@ -1,6 +1,7 @@
 #!/bin/sh
 
-switch_ip=10.0.0.13
+switch_ip=10.0.0.10
+switch_netmask=255.0.0.0
 gateway_ip=10.0.0.1
 controller_ip=10.0.0.1
 controller_port=6633
@@ -14,13 +15,14 @@ ofprotocol_options="--inactivity-probe=90"
 
 #--datapath-id must be exactly 12 hex digits
 id_num=`echo $switch_ip | awk -F '.' '{print $4}'`
-if [ $id_num -gt 99 ]
-then
-    datapath_id=000000000$id_num
-else
+if [ $id_num -lt 10 ]; then
+    datapath_id=00000000000$id_num
+elif [ $id_num -lt 100 ]; then
     datapath_id=0000000000$id_num
+else
+    datapath_id=000000000$id_num
 fi
-    
+
 echo "Networking Initial, Please wait..."
 # Due to driver issue, networking interfaces should be set up, then down at first.
 ifconfig eth1 up
@@ -35,14 +37,14 @@ ifconfig eth2 down
 ifconfig eth3 down
 ifconfig eth4 down
 
-ifconfig eth0 hw ether 00:0a:35:00:13:00
-ifconfig eth1 hw ether 00:0a:35:01:13:01
-ifconfig eth2 hw ether 00:0a:35:01:13:02
-ifconfig eth3 hw ether 00:0a:35:01:13:03
-ifconfig eth4 hw ether 00:0a:35:01:13:04
+ifconfig eth0 hw ether 00:0a:35:00:12:00
+ifconfig eth1 hw ether 00:0a:35:01:12:01
+ifconfig eth2 hw ether 00:0a:35:01:12:02
+ifconfig eth3 hw ether 00:0a:35:01:12:03
+ifconfig eth4 hw ether 00:0a:35:01:12:04
 sleep 1
 
-ifconfig eth0 $switch_ip up
+ifconfig eth0 $switch_ip netmask $switch_netmask up
 ifconfig eth1 up
 ifconfig eth2 up
 ifconfig eth3 up
@@ -70,16 +72,17 @@ cd $ofs_dir
 echo "Starting configuring udatapath..."
 ./udatapath/ofdatapath --datapath-id=$datapath_id --interfaces=$interfaces ptcp:$local_port $ofdatapath_options &
 sleep 3
-./utilities/dpctl tcp:127.0.0.1:6632 flow-mod cmd=add,table=0,prio=0 apply goto:1
-./utilities/dpctl tcp:127.0.0.1:6632 flow-mod cmd=add,table=1,prio=0 apply goto:2
-./utilities/dpctl tcp:127.0.0.1:6632 flow-mod cmd=add,table=2,prio=0 apply goto:3
-sleep 1
 ret=`ps | grep "ofdatapath" | awk '{print $6}'`
 if [ ! $ret ]
 then
     echo "ERROR: Excuting ofdatapath failed, please check..."
     exit
 fi
+
+#add default entry to be consistent with hardware pkt-in entry
+./utilities/dpctl tcp:127.0.0.1:6632 flow-mod cmd=add,table=0,prio=0 apply goto:1
+./utilities/dpctl tcp:127.0.0.1:6632 flow-mod cmd=add,table=1,prio=0 apply goto:2
+./utilities/dpctl tcp:127.0.0.1:6632 flow-mod cmd=add,table=2,prio=0 apply goto:3
 
 sleep 3
 
